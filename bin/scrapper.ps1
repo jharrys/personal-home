26e693c218423ac6fdb898aba4f8637304d803da
John Harris on D91M31M1/Cygwin <john.harris@imail.org>
2012-08-19 20:18:15 -0600

    scrap file changes. added my theme to config

--- bin/scrapper.ps1
+++ bin/scrapper.ps1
@@ -1,35 +1,100 @@
-﻿# Collects all named paramters (all others end up in $Args)
-#param($eventRecordID,$eventChannel)
-
-#$event = get-winevent -LogName "${eventChannel}" -FilterXPath "<QueryList>  <Query Id=`"0`" Path=`"${eventChannel}`"> <Select Path=`"${eventChannel}`">*[System[(EventRecordID=${eventRecordID})]]</Select> </Query> </QueryList>"
-#$event = get-winevent -LogName "Microsoft-Windows-NetworkProfile/Operational" -FilterXPath "<QueryList><Query Id=`"0`" Path=`"Microsoft-Windows-NetworkProfile/Operational`"> <Select Path=`"Microsoft-Windows-NetworkProfile/Operational`">*[System[(EventRecordID=2566)]]</Select> </Query> </QueryList>"
+﻿# Parameter options
+#param
+#	(
+#		$eventRecordID,
+#		$eventChannel,
+#		[Parameter(Mandatory=$true)]
+#		[AllowNull()]
+#		[AllowEmptyString()]
+#		[string]$networkName,
+#		$eventState
+#	)
+##########################################################
+# define a hashtable
+#[hashtable] $homeMounts = @{
+#	share="\\zax\mnt\nethome\john"
+#	drive="z:"
+#}
+##########################################################
+#$ipAddress = @()
+#$ipAddress = gwmi win32_NetworkAdapterConfiguration |
+#	? { $_.IPEnabled -eq $true } |
+#	% { $_.IPAddress } |
+#	% { [IPAddress]$_ } |
+#	? { $_.AddressFamily -eq 'internetwork' } |
+#	% { $_.IPAddressToString }
 #
-#[xml]$eventParams = [xml]($event.ToXml())
-#$eventParams.SelectNodes("/EventData")
-#$mename = $eventParams.SelectSingleNode("/Event/Data[@Name='Name']")
-
-#$mename
-
-
-# parseXML.ps1
-write-host "`nParsing file testCases.xml`n"
-[System.Xml.XmlDocument] $xd = new-object System.Xml.XmlDocument
-$file = resolve-path("c:\Users\lpjharri\bin\testCases.xml")
-#[xml]$xd = (Get-Content $file) -replace 'xmlns="http://schemas.microsoft.com/win/2004/08/events/event"'
-[xml]$xd = (Get-Content $file)
-$ns = New-Object Xml.XmlNamespaceManager $xd.NameTable
-$ns.AddNamespace( "e", "http://schemas.microsoft.com/win/2004/08/events/event" )
-# XPath is case sensitive
-$nodelist = $xd.selectnodes("/e:Event/e:EventData/e:Data[@Name='Name']", $ns) 
-foreach ($testCaseNode in $nodelist) {
-  $id = $testCaseNode."#text"
-  $id
-#  $inputsNode = $testCaseNode.selectSingleNode("inputs")
-#  $arg1 = $inputsNode.selectSingleNode("arg1").get_InnerXml()
-#  $optional = $inputsNode.selectSingleNode("arg1").getAttribute("optional")
-#  $arg2 = $inputsNode.selectSingleNode("arg2").get_InnerXml()
-#  $expected = $testCaseNode.selectSingleNode("expected").get_innerXml()
-#  #$expected = $testCaseNode.expected 
-#  write-host "Case ID = $id Arg1 = $arg1 Optional = $optional Arg2 = $arg2 Expected value = $expected"
-}
-write-host "`nEnd parsing`n"
+#Write-Host -fore cyan "Your current network is $ipAddress."
+##########################################################
+# Popup Types 
+# 0 Show OK button.
+# 1 Show OK and Cancel buttons.
+# 2 Show Abort, Retry, and Ignore buttons.
+# 3 Show Yes, No, and Cancel buttons.
+# 4 Show Yes and No buttons.
+# 5 Show Retry and Cancel buttons
+#$hey = new-object -comobject wscript.shell
+#$answer = $hey.popup($script:popupMsg,0,$script:popupTitle,$script:popupType)
+##########################################################
+#$homeNet = "10.1.4.*", "OfficePrinter"
+#$remoteNet = "10.1.6.*", "W382_HP_Printer"
+#
+#function Set-DefaultPrinter([string]$printerPath) {
+#	$printers = gwmi -class Win32_Printer -computer .
+#	Write-Host -fore cyan "Default Printer: $printerPath"
+#	$dp = $printers | ? { $_.deviceID -match $printerPath }
+#	$dp.SetDefaultPrinter() | Out-Null
+#}
+#
+#$ipAddress = @()
+#$ipAddress = gwmi win32_NetworkAdapterConfiguration |
+#	? { $_.IPEnabled -eq $true } |
+#	% { $_.IPAddress } |
+#	% { [IPAddress]$_ } |
+#	? { $_.AddressFamily -eq 'internetwork' } |
+#	% { $_.IPAddressToString }
+#
+#Write-Host -fore cyan "Your current network is $ipAddress."
+#
+#switch -wildcard ($ipAddress) {
+#	$homeNet[0] { Set-DefaultPrinter $homeNet[1] }
+#	$remoteNet[0] { Set-DefaultPrinter $remoteNet[1] }
+#	default { Set-DefaultPrinter $homeNet[1] }
+#}
+##########################################################
+# get printer object (System.Management.ManagementObject#root\cimv2\Win32_Printer)
+#$printer = Get-WmiObject -Class Win32_Printer
+# (gwmi -computername . -class win32_printer -filter "name like 'Dad%'").setdefaultprinter()
+##########################################################
+# personalize desktop for restricted user
+#function CreateRestrictedDesktopGPO 
+#{
+#Param
+#   (
+#    [string]$gpoName,
+#    [string]$themeFileName,
+#    [string]$wallpaperFileName
+#    )
+#
+#    $gpo = New-GPO -Name $gpoName
+#
+#    #set the policy to specify a theme
+#    $gpo | Set-GPRegistryValue -Key "HKCU\Software\Policies\Microsoft\Windows\Personalization" -Type String `
+#    -ValueName ThemeFile -Value $themeFileName
+#
+#    #set the policy to prevent the user from changing the theme
+#    $gpo | Set-GPRegistryValue -Key "HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer" `
+#    -Type Dword -ValueName NoThemesTab -Value 1
+#
+#    #set the policy to specify a wallaper. This policy also prevents the user from changing the wallpaper
+#    $gpo | Set-GPRegistryValue -Key "HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\System" `
+#    -Type String -ValueName Wallpaper -Value $wallpaperFileName
+#}
+##########################################################
+# How to load assembly
+#[System.Reflection.Assembly]::LoadWithPartialName("Microsoft.SharePoint")
+##########################################################
+# get battery status
+#(Get-WmiObject -Class Win32_Battery -ea 0).BatteryStatus
+##########################################################
+#powershell -nologo -command (invoke-command -scriptblock {"C:\Users\lpjharri\AppData\Local\Microsoft\Windows\Themes\john.theme"})
