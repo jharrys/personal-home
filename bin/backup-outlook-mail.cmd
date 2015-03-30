851cd66926d64c3c2d8ffaa75effa133a3dcf291
Johnnie Harris <johnnie.harris@imail.org>
2015-03-30 13:36:09 -0600

    Updated backup outlook mail.

--- bin/backup-outlook-mail.cmd
+++ bin/backup-outlook-mail.cmd
@@ -1,3 +1,8 @@
 :: Johnnie Harris 2015
 :: Needs to be run as Administrator
-robocopy \Users\lpjharri\Documents\MailArchives\ X:\MailArchives /MT:64 /ZB
+
+:: MT - Multiple Threads
+:: Z - run in restartable mode (if interrupted it can restart where it left off)
+:: B - run in Backup mode (open files can be backed up)
+
+robocopy \Users\lpjharri\Documents\MailArchives\ \\co.ihc.com\root\CO\TL\User\lpjharri\MailArchives /MT:64 /ZB
