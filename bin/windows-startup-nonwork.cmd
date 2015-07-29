4c560cc8569b065e1b668b5c510d6ffd8b061d1d
Johnnie Harris <johnnie.harris@imail.org>
2015-07-29 14:23:33 -0600

    Adding startup scripts and cron jobs for windows.

---
+++ bin/windows-startup-nonwork.cmd
@@ -0,0 +1,4 @@
+SET drive_dir="%ProgramFiles(x86)%\Google\Drive"
+cd %drive_dir%
+
+start googledrivesync.exe
---
+++ bin/windows-startup-work.cmd
@@ -0,0 +1,5 @@
+SET office_dir="%ProgramFiles(x86)%\Microsoft Office\Office15"
+cd %office_dir%
+
+start OUTLOOK.EXE
+start lync.exe
