ab148ab01fca19933ed2b46b01da827d52158cba
Johnnie Harris <johnnie.harris@imail.org>
2017-01-17 21:58:30 -0700

    Updated pchanger to handle superuser files.

--- .bin/pchanger.sh
+++ .bin/pchanger.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 FILES='$HOME/tmp/creds $HOME/tmp/.corkscrew-auth $HOME/tmp/settings.xml $HOME/.npmrc'
+SFILES='/etc/auto_johnnie'
 OLDPASS=$1
 NEWPASS=$2
 
@@ -14,3 +15,15 @@ do
     echo "no such file ${filename}"
   fi
 done
+
+for sf in $SFILES;
+do
+  eval sfilename=$sf
+  if [ -r "${sfilename}" ];
+  then
+    sudo sed -i .bak "s/${OLDPASS}/${NEWPASS}/g" $sfilename
+  else
+    echo "no such file ${sfilename}"
+  fi
+done
+
