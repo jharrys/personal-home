8d104fb6ca79565717bf41ed7de3f7f508c3c8ff
Johnnie Harris <johnnie.harris@imail.org>
2017-04-08 02:56:21 -0600

    Made the output more colorful.

--- .bin/brewcasklist.sh
+++ .bin/brewcasklist.sh
@@ -1,28 +1,28 @@
-#!/bin/zsh
+#!/bin/sh
 #
-usage="usage:\t$0 \n\t$0 toupdate|tu"
 
-red=`tput setaf 1`
-green=`tput setaf 2`
-yellow=`tput setaf 3`
-blue=`tput setaf 4`
-magenta=`tput setaf 5`
-cyan=`tput setaf 6`
-white=`tput setaf 7`
-reset=`tput sgr0`
+# source my colors file
+. colors.sh
+
+usage="usage:\t$0 \n\t$0 toupdate|tu"
 
 LISTING=$(brew cask ls -1)
 
 if [ "$1" = "-h" -o "$1" = "--help" ]; then
-  echo $usage && return 1
+  $colored_fg_magenta
+  echo $usage && exit 1
 elif [ "$1" = "toupdate" -o "$1" = "tu" ]; then
-  tput setaf 3
+  $colored_fg_yellow
   echo $LISTING |grep -v "\!" |xargs brew cask info |sed -n -f ~/.bin/brewcask.sed |awk -v RS="" '{ if ($3 != $6) print $2, "needs to be upgraded" }'
-  tput sgr0
+  $type_reset
 
 elif [ $# -lt 1 ]; then
+  $colored_fg_green
   echo $LISTING |grep -v "\!" |xargs brew cask info |sed -n -f ~/.bin/brewcask.sed
+  $type_reset
 fi
 
 #echo "Apps no longer in homebrew/cask:"
-#echo $LISTING|grep "\!"
+$colored_fg_red
+echo $LISTING|grep "\!"
+$type_reset
