701040079640ad3952686db739bfd0d1e85c7789
Johnnie Harris <johnnie.harris@imail.org>
2017-04-24 17:21:40 -0600

    Updated colors.sh with a function to display all colors.

--- .bin/colors.sh
+++ .bin/colors.sh
@@ -31,3 +31,17 @@ type_rev='tput rev'                       # reverse mode on
 type_reset='tput sgr0'                    # clear all attributes
 cursor_hide='tput civis'                  # hide cursor
 cursor_normal='tput cnorm'                # normal cursor
+
+function display-my-colors {
+  for x in {0..8}; do
+    for i in {30..37}; do
+        for a in {40..47}; do
+            echo -ne "\e[$x;$i;$a""m\\\e[$x;$i;$a""m\e[0;37;40m "
+        done
+        echo
+    done
+  done
+
+  echo ""
+
+}
