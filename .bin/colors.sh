e8ccc90db740e26d81aadcc5c8c61bbb019a73ea
Johnnie Harris <johnnie.harris@imail.org>
2017-04-25 17:42:14 -0600

    Fixed function declaration in colors.sh - name was not legal.

--- .bin/colors.sh
+++ .bin/colors.sh
@@ -32,7 +32,7 @@ type_reset='tput sgr0'                    # clear all attributes
 cursor_hide='tput civis'                  # hide cursor
 cursor_normal='tput cnorm'                # normal cursor
 
-function display-my-colors {
+function displaycolors () {
   for x in {0..8}; do
     for i in {30..37}; do
         for a in {40..47}; do
