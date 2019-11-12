510feeb04fb76cdd8203036e8b846f198344607d
Johnnie Harris <harris.johnny@gmail.com>
2019-11-11 18:57:59 -0700

    deleted scripts no longer used.

--- .bin/windows-startup-nonwork.cmd
+++
@@ -1,4 +0,0 @@
-SET drive_dir="%ProgramFiles(x86)%\Google\Drive"
-cd %drive_dir%
-
-start googledrivesync.exe
--- .bin/windows-startup-work.cmd
+++
@@ -1,8 +0,0 @@
-SET office_dir="%ProgramFiles(x86)%\Microsoft Office\Office15"
-cd %office_dir%
-
-start OUTLOOK.EXE
-start lync.exe
-
-cd %userprofile%\Documents\Applications\OWAtray\
-start .\DrunkenBakery.OWAtray.GUI.exe
