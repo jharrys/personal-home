0df757611b3e2fe72c2465b5fcefb3c623917b19
Johnnie Harris <harris.johnny@gmail.com>
2020-08-02 19:10:41 -0600

    Updates to bcu.sh; added png files for intellij; update to bindkey.

--- .bin/bcu.sh
+++ .bin/bcu.sh
@@ -1,7 +1,19 @@
 #!/bin/sh
 # runs all brew commands to update itself, upgrade installed non-cask apps and clean itself
+today="$(date '+%F')"
+exists=0
+
 file="$(date '+%F-%H%M')-brew-update.log"
-file=/Users/jharris/tmp/bcu/${file}
+file=/Users/jharrys/tmp/bcu/${file}
+
+for x in /Users/jharrys/tmp/bcu/*.log
+do
+  if [ ${x%-????-brew-update.log} = "/Users/jharrys/tmp/bcu/$today" ]
+  then
+    exit 0
+  fi
+done
+
 mkdir -p $(dirname $file)
 echo "*** update of brew" > $file
 /usr/local/bin/brew update >> $file
@@ -9,4 +21,3 @@ echo "*** upgrade of brew" >> $file
 /usr/local/bin/brew upgrade >> $file
 echo "*** cleanup of brew" >> $file
 /usr/local/bin/brew cleanup >> $file
-
